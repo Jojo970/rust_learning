64cbcb481ca8952a16b638e2fbdca04f9e50fb73
jong lee <jonglee@jongs-Air.attlocal.net>
2024-03-07 16:29:07 -0600

    variables

---
+++ variables/target/debug/deps/variables-f5d32ddd8ebb9686.d
@@ -0,0 +1,5 @@
+/Users/jonglee/Desktop/CodingProjects/rust_learning/variables/target/debug/deps/variables-f5d32ddd8ebb9686: src/main.rs
+
+/Users/jonglee/Desktop/CodingProjects/rust_learning/variables/target/debug/deps/variables-f5d32ddd8ebb9686.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/jonglee/Desktop/CodingProjects/rust_learning/variables/target/debug/variables: /Users/jonglee/Desktop/CodingProjects/rust_learning/variables/src/main.rs
